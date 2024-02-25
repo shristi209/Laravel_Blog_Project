f6afc90e68b4e7d258cfdd6995c8eea0f2cb52d2
shristi209 <shristichapagain98@gmail.com>
2024-02-25 05:47:49 +0545

    Performed getall, getbyid and update blogs.

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -16,4 +16,24 @@ class ApiController extends Controller
         $blogs->save();
         return response()->json($blogs); // returning student
     }
+    public function read()
+    {
+        $blogs = blogmodel::all();  
+        return response()->json($blogs);
+    }
+    public function readbyid($id)
+    {
+        $blogs=blogmodel::find($id);
+        return response()->json($blogs);
+    }
+    public function updatebyid(Request $request,$id)
+    {
+        $blogs=blogmodel::find($id);
+        $blogs->title=$request->input('title');
+        $blogs->description=$request->input('description');
+        $blogs->category=$request->input('category');
+
+        $blogs->save();
+        return response()->json($blogs); 
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 //We are creating a POST method to insert the data. 
-Route::post('/blog', [ApiController::class, 'create']);// This line point create function in the blog_api_controller(This will handle the post method);
+Route::post('/blogcreate', [ApiController::class, 'create']);// This line point create function in the blog_api_controller(This will handle the post method);
 
+Route::get('/blogall', [ApiController::class, 'read']);
+
+Route::get('/blogbyid/{id}', [ApiController::class, 'readbyid']);
+
+Route::put('/blogupdate/{id}',[ApiController::class,'updatebyid']);
